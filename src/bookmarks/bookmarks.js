4d9ca668b4d3c6362be050066ff3eb6403ec7303
Kevin Boyle <papershack@gmail.com>
2019-08-07 20:16:39 -0700

    add logger.js, component dir for bookmarks router, store of data

---
+++ src/bookmarks/bookmarks.js
@@ -0,0 +1,4 @@
+const app = require('express')
+
+
+const bookmarksRouter = express.Router()
---
+++ src/store.js
@@ -0,0 +1,46 @@
+const bookmarks = [
+    {
+        "id": "cjozyzcil0000lxygs3gyg2mr",
+        "title": "Thinkful",
+        "url": "https://www.thinkful.com",
+        "description": "Think outside the classroom",
+        "rating": 5
+    },
+    {
+        "id": "cjozyzeqh0001lxygb8mhnvhz",
+        "title": "Google",
+        "url": "https://www.google.com",
+        "description": "Where we find everything else",
+        "rating": 4
+    },
+    {
+        "id": "cjkzyzeqh0001lxygb8mhqvh3",
+        "title": "MDN",
+        "url": "https://developer.mozilla.org",
+        "description": "The only place to find web documentation",
+        "rating": 5
+    },
+    {
+        "id": "cjz1toix7000304wvzo6af8ma",
+        "title": "Talent Acquisition Recruiter",
+        "url": "https://Google.com",
+        "description": "TESTTTTT",
+        "rating": 1
+    },
+    {
+        "id": "cjz1vvbsy000404wvl46fv24x",
+        "title": "this",
+        "url": "https://is.com",
+        "description": "weird",
+        "rating": 1
+    },
+    {
+        "id": "cjz1yg36j000504wvj3trusdr",
+        "title": "Zach's Yoga",
+        "url": "https://courses.thinkful.com/react-v1/checkpoint/14",
+        "description": "Its gooooood",
+        "rating": 5
+    }
+]
+
+module.exports = bookmarks
